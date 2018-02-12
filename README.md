45ccbc5ab0125b3ef6eaefb08160c572b62d64d6
igqueiroz <andyarm@gmail.com>
2018-02-12 16:05:58 -0200

    First View

---
+++ README.md
@@ -0,0 +1,47 @@
+# Geo Location App
+Este projeto é uma App baseado em Google Maps que coleta dados do usuário, cria uma API simples em REST e mostra lugares num raio escolhido pelo usuário.
+
+Stack usado na aplicação:
+
+## Client Side
+> Create React App (React, Webpack, Autoprefixer, JSX, Babel)
+> Redux
+> Router
+> SASS
+> Bootstrap
+
+## Server Side
+> NodeJs
+> Express
+> Jade
+
+## Database
+> MongoDB
+
+A documentação de cada componente registrada diretamente no código
+
+## Como rodar o projeto
+
+1. Faça o clone do projeto ou baixe o Zip na sua máquina (escolha um caminho de pasta sem espaços e caixa alta para evitar Warnings indesejados)
+
+2. Abra um console e execute o comando para rodar o Mongo Daemon [baixe aqui](https://www.mongodb.com/download-center?jmp=nav#community), digite: 
+```mongod```
+
+3. Em um novo console, abra a pasta 'server' e digite:
+```npm install```
+```npm start```
+
+4. Em um terceiro console, abra a pasta 'client' e digite
+```npm install```
+```npm start```
+
+5. Para gerar o build, abra a pasta 'client' e digite
+```npm build```
+
+## Locais
+
+localhost:3000 - Front-End
+localhost:3001 - Back-End
+localhost:3001/userlist - API Rest
+
+A interação do usuário vai gerar dados acessados pela API.
---
+++ server/routes/index.js
@@ -0,0 +1,46 @@
+// Arquivo que cria as rotas manipula as interações do usuário com o banco Express
+// Cria as APIS consumíveis e retorna ao usuário dados de consulta do banco 
+
+var express = require('express');
+var router = express.Router();
+var assert = require('assert');
+
+//Cria a API de lista de usuários coletados
+router.get('/userlist', (req, res) => {
+    let db = req.db.collection('users');
+    db.find({})
+      .then(users => res.send(users))
+      .catch(error => {
+      	console.error(error);
+		res.status(404).send('Bad Request');
+     });
+});
+
+// Permite a interação via POST, recebe os dados da App e envia para o servidor MongoDB
+router.post('/userlist', (req, res) => {
+	let db = req.db.collection('users');
+	const name = req.body.newName;
+	const email = req.body.newEmail;
+	const userLocationLat = req.body.newUserLocationLat;
+	const userLocationLng = req.body.newUserLocationLng;
+	const userDevice = req.body.newUserDevice;
+	const userRange = req.body.newRange;
+	const universityLat = req.body.newUniversityLat;
+	const universityLng = req.body.newuniversityLng;
+	
+	// Quando tiver validação de dados no lado do servidor, colocar nesse espaço
+
+	// Insere os dados coletados via POST vindos da App no nosso banco 
+	db.insert({
+		'name': name,
+		'email': email,
+		'userLocationLat': userLocationLat,
+		'userLocationLng': userLocationLng,
+		'userDevice': userDevice,
+		'userRange': userRange,
+		'universityLat': universityLat,
+		'universityLng': universityLng
+	})
+});
+   
+module.exports = router;
